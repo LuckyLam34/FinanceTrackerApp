bbe700b5e7a945e88168131dc7c59dd158e7213b
Lucky <tutrinh3004@gmail.com>
2018-05-23 22:35:14 +0700

    add moment and jquery

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,10 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+  }
 
+  ionViewDidLoad() {
+    console.log($('#event-calendar'));
   }
 
 }
